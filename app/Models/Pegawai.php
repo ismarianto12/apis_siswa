62e7911847e48b9e856a5aaad0cfeee064db65f3
Harris <harris>
2022-12-24 11:32:21 +0700

    first commit

---
+++ app/Models/Pegawai.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Support\Facades\DB;
+
+class Pegawai extends model
+{
+
+    protected $table = 'pegawai';
+    public $timestamps = false;
+}
